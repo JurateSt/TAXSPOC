8b530202d8718074872c2da6a35e7e23933d1081
Jurate St <jurate@sdhouse.lt>
2024-06-15 00:20:55 +0300

    Add click functionality to logo in NavigationBar component

--- ui-web/src/components/NavigationBar.jsx
+++ ui-web/src/components/NavigationBar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 // MUI Components
 import { Box, Toolbar } from '@mui/material';
 // Logo
@@ -6,8 +7,15 @@ import MainLogo from '../assets/MainLogo.png';
 // MUI Icons
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import NavigationLink from './NavigationLink';
+import { on } from 'events';
 
 const NavigationBar = () => {
+	const navigate = useNavigate();
+
+	const handleClick = () => {
+		navigate('/home');
+	};
+
 	const hotTopicsMenuItems = [
 		{ label: 'Hot Topics Europe', path: '/hot-topics' },
 		{ label: 'Hot Topics USA', path: '/hot-topics-usa' },
@@ -27,7 +35,9 @@ const NavigationBar = () => {
 					alignItems: 'center', // Ensures logo is vertically centered
 					marginRight: '128px', // Space between logo and navigation links
 					userSelect: 'none',
+					cursor: 'pointer',
 				}}
+				onClick={handleClick}
 			>
 				<img src={MainLogo} alt="TaxSpoc Logo" />
 			</Box>
