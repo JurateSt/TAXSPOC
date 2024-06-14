c5777feb0b9e9250b9c54a17d3c23cbe9615e0c5
ProjectFourtyTwo <etpkps42@gmail.com>
2024-06-14 18:36:43 +0000

    Merged in fix-2024-06-14 (pull request #3)
    Click fixes in article card
    
    * Click fixes in article card

--- ui-web/src/components/Article/ArticleCardPhoto.jsx
+++ ui-web/src/components/Article/ArticleCardPhoto.jsx
@@ -1,9 +1,20 @@
+import { useNavigate } from 'react-router-dom';
 // MUI
 import { Grid } from '@mui/material';
 
 const ArticleCardPhoto = ({ article }) => {
+	const navigate = useNavigate();
+	const { _id: id } = article;
+	const handleClick = () => {
+		navigate(`/articles/${id}`);
+	};
 	return (
-		<Grid item xs={12} sx={{ bgcolor: 'lightblue', height: '100px' }}>
+		<Grid
+			item
+			xs={12}
+			sx={{ bgcolor: 'lightblue', height: '100px', cursor: 'pointer' }}
+			onClick={handleClick}
+		>
 			<img
 				src={article?.images[0]?.url}
 				style={{ width: '100%', height: '100%', objectFit: 'cover' }}
