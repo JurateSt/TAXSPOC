26e140a74e5fd907f6530d55b433de3f90f1d5de
Jurate St <jurate@sdhouse.lt>
2024-06-23 16:42:03 +0300

    Enable paste as plain text in TinyMCE editor

--- ui-web/src/components/TinyMCE/EditorTinyMCE.jsx
+++ ui-web/src/components/TinyMCE/EditorTinyMCE.jsx
@@ -7,9 +7,10 @@ const EditorTinyMCE = ({ value, onChange }) => {
 		<Editor
 			apiKey="rk2aip84g35omv0hc2cscaxth7rtdo001kbuxl6ndao4f68b"
 			init={{
+				paste_as_text: true,
 				plugins:
 					'preview importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media codesample table charmap pagebreak nonbreaking anchor insertdatetime advlist lists wordcount help charmap quickbars emoticons accordion',
-					// 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage advtemplate ai mentions tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss markdown',
+				// 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage advtemplate ai mentions tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss markdown',
 				toolbar:
 					'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | checklist numlist bullist indent outdent | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight  | emoticons charmap | removeformat',
 				tinycomments_mode: 'embedded',
