86ef27a0cdd3a4365b5776a3bdddf86665fd5bb8
Jurate St <jurate@sdhouse.lt>
2024-06-23 16:51:59 +0300

    disable paste as plain text in TinyMCE editor

--- ui-web/src/components/TinyMCE/EditorTinyMCE.jsx
+++ ui-web/src/components/TinyMCE/EditorTinyMCE.jsx
@@ -7,7 +7,7 @@ const EditorTinyMCE = ({ value, onChange }) => {
 		<Editor
 			apiKey="rk2aip84g35omv0hc2cscaxth7rtdo001kbuxl6ndao4f68b"
 			init={{
-				paste_as_text: true,
+				paste_as_text: false,
 				plugins:
 					'preview importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media codesample table charmap pagebreak nonbreaking anchor insertdatetime advlist lists wordcount help charmap quickbars emoticons accordion',
 				// 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage advtemplate ai mentions tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss markdown',
