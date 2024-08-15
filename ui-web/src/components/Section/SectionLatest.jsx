88d10f22e1799d21d69b6ad23b85835eb6ae7baa
ProjectFourtyTwo <etpkps42@gmail.com>
2024-08-15 16:10:43 +0000

    Merged in articles-list-ux (pull request #63)
    Articles list ux
    
    * Remove buttons from ArticleCard and MainArticle components
    
    * return main article button
    
    * Merge branch 'master' into articles-list-ux
    
    * fix suggested articles repeating and add click to bottom logo
    
    * Merge branch 'master' into articles-list-ux
    
    * Add lazy loading for article card and main article photos
    
    * Merge branch 'master' into articles-list-ux
    
    * Remove commented out console.log statement in Home.jsx
    
    * Category Chips working
    
    * update favicon to use new logo
    
    * Merge branch 'master' into articles-list-ux
    
    * new design 2024-08-12
    
    * Merge branch 'master' into articles-list-ux
    
    * Refactor route for latest articles to use hyphenated URL
    
    * mobile design fixes
    
    * Update .gitignore and add new logo files
    
    * Merge branch 'master' into articles-list-ux

--- ui-web/src/components/Section/SectionLatest.jsx
+++ ui-web/src/components/Section/SectionLatest.jsx
@@ -10,7 +10,7 @@ import ArticleCard from '../Article/ArticleCard';
 import ArticleShortCard from '../Article/ArticleShortCard';
 
 const Section = ({ section, onLoaded }) => {
-	const isMobile = useMediaQuery((theme) => theme.breakpoints.down('md'));
+	// const isMobile = useMediaQuery((theme) => theme.breakpoints.down('md'));
 
 	const [articles, setArticles] = useState([]);
 	const [mainArticle, setMainArticle] = useState({});
@@ -32,17 +32,18 @@ const Section = ({ section, onLoaded }) => {
 		articles.length > 0 && (
 			<Grid
 				container
+				rowSpacing={1}
 				sx={{
 					borderBottom: '1px solid',
 					borderColor: 'primary.divider',
 					// border: '1px solid blue',
 					// padding same as for category section title
-					padding: '12px 0',
+					padding: '16px 0',
 				}}
 			>
 				<SectionCategory category={section.category} />
 
-				<Grid container item xs={12} columnSpacing={2}>
+				<Grid container item xs={12} columnSpacing={2} rowSpacing={2}>
 					<Grid
 						container
 						item
@@ -56,15 +57,11 @@ const Section = ({ section, onLoaded }) => {
 						// 	border: '1px solid blue',
 						// }}
 					>
-						{isMobile ? (
-							<ArticleCard article={mainArticle} />
-						) : (
-							<MainArticle article={mainArticle} />
-						)}
+						<MainArticle article={mainArticle} />
 
-						<Grid container item xs={12} columnSpacing={2}>
+						<Grid container item xs={12} columnSpacing={2} rowSpacing={2}>
 							{articles.slice(1, 4).map((item, index) => (
-								<Grid key={index} item xs={12} sm={12} md={4} lg={4} xl={4}>
+								<Grid key={index} item xs={12} sm={6} md={4} lg={4} xl={4}>
 									<ArticleCard key={index} article={item} />
 								</Grid>
 							))}
@@ -74,30 +71,26 @@ const Section = ({ section, onLoaded }) => {
 					<Grid
 						container
 						item
-						// rowSpacing={2}
+						rowSpacing={2}
 						xs={12}
 						sm={12}
 						md={4}
 						lg={4}
 						xl={4}
 						//start from flex start
-						sx={{
-							display: 'flex',
-							flexDirection: 'column',
-							justifyContent: 'flex-start',
-							gap: '12px',
-							// border: '1px solid green',
-						}}
+						// sx={{
+						// 	display: 'flex',
+						// 	flexDirection: 'column',
+						// 	justifyContent: 'flex-start',
+						// 	// gap: '48px',
+						// 	// border: '1px solid green',
+						// }}
 					>
-						{articles.slice(4, 8).map(
-							(item, index) => (
-								// isMobile ? (
-								// 	<ArticleCard key={index} article={item} />
-								// ) : (
+						{articles.slice(4, 8).map((item, index) => (
+							<Grid container item key={index} xs={12}>
 								<ArticleShortCard key={index} article={item} index={index} />
-							)
-							// )
-						)}
+							</Grid>
+						))}
 					</Grid>
 				</Grid>
 			</Grid>
