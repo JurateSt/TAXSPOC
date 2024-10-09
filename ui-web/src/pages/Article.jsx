7ccacab0b1aea5642bc8347d0d01895104809e7d
Jurate St <jurate@sdhouse.lt>
2024-10-09 17:25:00 +0300

    test metatags

--- ui-web/src/pages/Article.jsx
+++ ui-web/src/pages/Article.jsx
@@ -55,10 +55,10 @@ const Article = () => {
 			<Helmet>
 				<link rel="canonical" href={`https://www.taxspoc.com/articles/${article.slug}`} />
 				<title>{article.header || 'Article'}</title>
-				<meta name="title" content={article.header || 'Article'} />
+				<meta name="title" content={`Taxspoc: ${article.header}` || 'Article'} />
 				{/* Open Graph tags */}
 				<meta property="og:type" content="article" />
-				<meta property="og:title" content={article.header || 'Article'} />
+				<meta property="og:title" content={`Taxspoc: ${article.header}` || 'Article'} />
 				<meta
 					property="og:description"
 					content={
@@ -66,7 +66,7 @@ const Article = () => {
 						'Taxspoc is your Single Point of Contact for global and local tax news, providing clear, logical, and well-organized tax information'
 					}
 				/>
-				<meta property="og:image" content={article?.images?.[0]?.url} />
+				<meta property="og:image" content={`${article?.images?.[0]?.url}`} />
 				<meta property="og:url" content={`https://www.taxspoc.com/articles/${article.slug}`} />
 				{/* Twitter Card tags */}
 				<meta name="twitter:card" content="summary_large_image" />
