e2f57933100a1aac18ae531ec55a4fb214e2e99e
Jurate St <jurate@sdhouse.lt>
2024-10-09 17:42:03 +0300

    test metatags

--- ui-web/src/pages/Article.jsx
+++ ui-web/src/pages/Article.jsx
@@ -54,30 +54,18 @@ const Article = () => {
 		<>
 			<Helmet>
 				<link rel="canonical" href={`https://www.taxspoc.com/articles/${article.slug}`} />
-				<title>{article.header || 'Article'}</title>
-				<meta name="title" content={`Taxspoc: ${article.header}` || 'Article'} />
+				<title>{article.header}</title>
+				<meta name="title" content={`Taxspoc: ${article.header}`} />
 				{/* Open Graph tags */}
 				<meta property="og:type" content="article" />
-				<meta property="og:title" content={`Taxspoc: ${article.header}` || 'Article'} />
-				<meta
-					property="og:description"
-					content={
-						article.supportingText ||
-						'Taxspoc is your Single Point of Contact for global and local tax news, providing clear, logical, and well-organized tax information'
-					}
-				/>
+				<meta property="og:title" content={`Taxspoc: ${article.header}`} />
+				<meta property="og:description" content={article.supportingText} />
 				<meta property="og:image" content={`${article?.images?.[0]?.url}`} />
 				<meta property="og:url" content={`https://www.taxspoc.com/articles/${article.slug}`} />
 				{/* Twitter Card tags */}
 				<meta name="twitter:card" content="summary_large_image" />
-				<meta name="twitter:title" content={article.header || 'Article'} />
-				<meta
-					name="twitter:description"
-					content={
-						article.supportingText ||
-						'Taxspoc is your Single Point of Contact for global and local tax news, providing clear, logical, and well-organized tax information'
-					}
-				/>
+				<meta name="twitter:title" content={article.header} />
+				<meta name="twitter:description" content={article.supportingText} />
 				<meta name="twitter:image" content={article?.images?.[0]?.url} />
 			</Helmet>
 
