6169da3f2104633ccae9262343488422c7f2570f
Jurate St <jurate@sdhouse.lt>
2024-10-11 19:08:01 +0300

    post build htmls

--- ui-web/src/pages/Article.jsx
+++ ui-web/src/pages/Article.jsx
@@ -53,27 +53,20 @@ const Article = () => {
 	return (
 		<>
 			<Helmet>
-				<title>{article.header || 'Article'}</title>
-				<meta name="title" content={article.header || 'Article'} />
-				<meta property="og:title" content={article.header || 'Article'} />
-				<meta property="og:description" content={article.supportingText || 'Article description'} />
-				<meta property="og:locale" content="en_US" />
+				<title>{article.header}</title>
+				<meta name="title" content={article.header} />
+				<meta property="og:title" content={article.header} />
+				<meta property="og:description" content={article.supportingText} />
 				<meta property="og:type" content="article" />
-				<meta property="og:image" content={article?.images?.[0]?.url || 'default-image-url.jpg'} />
+				<meta property="og:image" content={article?.images?.[0]?.url} />
 				<meta property="og:url" content={`https://www.taxspoc.com/articles/${article.slug}`} />
 				{/* <meta name="twitter:card" content="summary_large_image" /> */}
-				<meta name="twitter:title" content={article.header || 'Article'} />
-				<meta
-					name="twitter:description"
-					content={article.supportingText || 'Article description'}
-				/>
-				<meta name="twitter:image" content={article?.images?.[0]?.url || 'default-image-url.jpg'} />
+				<meta name="twitter:title" content={article.header} />
+				<meta name="twitter:description" content={article.supportingText} />
+				<meta name="twitter:image" content={article?.images?.[0]?.url} />
 
-				<meta
-					name="twitter:description"
-					content={article.supportingText || 'Article description'}
-				/>
-				<meta name="twitter:image" content={article.images?.[0]?.url || 'default-image-url.jpg'} />
+				<meta name="twitter:description" content={article.supportingText} />
+				<meta name="twitter:image" content={article.images?.[0]?.url} />
 			</Helmet>
 
 			<MainBar />
