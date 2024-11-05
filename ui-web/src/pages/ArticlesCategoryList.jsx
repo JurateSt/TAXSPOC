eaeb924c72a113593f0bbb4ed710911ba0778a69
Jurate St <jurate@sdhouse.lt>
2024-11-05 17:31:53 +0200

    load all articles

--- ui-web/src/pages/ArticlesCategoryList.jsx
+++ ui-web/src/pages/ArticlesCategoryList.jsx
@@ -22,7 +22,7 @@ const ArticlesCategoryList = ({}) => {
 	const categoryName = searchParams.get('category');
 
 	const [page, setPage] = useState(1);
-	const articlesPerPage = 8;
+	const articlesPerPage = 256;
 
 	const handleLoadMore = () => {
 		setPage((prev) => prev + 1);
