b309c355c3d966a0793a69d2e6fa8f5f495e75f3
ProjectFourtyTwo <etpkps42@gmail.com>
2025-02-10 20:27:39 +0000

    Merged in image-meta-data (pull request #196)
    In ArticleCardPhoto add figure
    
    * In ArticleCardPhoto add figure

--- ui-web/src/components/Article/ArticleCardPhoto.jsx
+++ ui-web/src/components/Article/ArticleCardPhoto.jsx
@@ -1,44 +1,43 @@
-// import { useNavigate } from 'react-router-dom';
 // MUI
 import { Grid } from '@mui/material';
 
 const VITE_BASE_URL = import.meta.env.VITE_BASE_URL;
 
 const ArticleCardPhoto = ({ article }) => {
-	// const navigate = useNavigate();
 	const { slug } = article;
 	const url = `/articles/${slug}`;
 	const imageSrc = article ? article?.images[0]?.url : '';
-	// const handleClick = () => {
-	// 	navigate(url);
-	// };
+	const imageAlt = article?.images[0]?.alt || 'Article Image';
 
 	return (
 		<Grid
 			item
 			xs={12}
 			sx={{
-				// bgcolor: 'lightblue',
-				// height: '100px',
 				'&:hover a': {
 					opacity: 0.7,
 					// transition: 'opacity 0.3s ease-in-out',
 				},
 			}}
-			// onClick={handleClick}
 		>
-			<a href={`${VITE_BASE_URL}${url}`} style={{ textDecoration: 'none', color: 'inherit' }}>
-				<img
-					src={imageSrc}
-					style={{
-						width: '100%',
-						height: '100%',
-						aspectRatio: '16/9',
-						objectFit: 'cover',
-					}}
-					alt="Article"
-					loading="lazy"
-				/>
+			<a
+				href={`${VITE_BASE_URL}${url}`}
+				title={article?.header || 'Read full article'}
+				style={{ textDecoration: 'none', color: 'inherit' }}
+			>
+				<figure style={{ margin: 0 }}>
+					<img
+						src={imageSrc}
+						style={{
+							width: '100%',
+							height: '100%',
+							aspectRatio: '16/9',
+							objectFit: 'cover',
+						}}
+						alt={imageAlt}
+						loading="lazy"
+					/>
+				</figure>
 			</a>
 		</Grid>
 	);
